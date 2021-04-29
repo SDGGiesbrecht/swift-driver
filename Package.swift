8a3377e1abb04464543fc93b11d4f2d846b4e46d
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2021-04-28 23:18:15 -0600

    Pinned swift‐tools‐support‐core at 0.2.0.

--- Package.swift
+++ Package.swift
@@ -96,7 +96,7 @@ if ProcessInfo.processInfo.environment["SWIFT_DRIVER_LLBUILD_FWK"] == nil {
 
 if ProcessInfo.processInfo.environment["SWIFTCI_USE_LOCAL_DEPS"] == nil {
   package.dependencies += [
-    .package(url: "https://github.com/apple/swift-tools-support-core.git", .branch("release/5.4")),
+    .package(url: "https://github.com/apple/swift-tools-support-core.git", .upToNextMinor(from: "0.2.0")),
     .package(url: "https://github.com/jpsim/Yams.git", .upToNextMinor(from: "4.0.0")),
     // The 'swift-argument-parser' version declared here must match that
     // used by 'swift-package-manager' and 'sourcekit-lsp'. Please coordinate
